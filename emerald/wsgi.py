efc66ea9db33968298dbcc32d612813c9fc2b782
Zac Yauney <zac.yauney@gmail.com>
2016-05-16 17:23:04 -0700

    Trying to fix wsgi

--- emerald/wsgi.py
+++ emerald/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 # Add to python path
-sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
+# sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
 
 
 
