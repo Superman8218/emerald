57d44664720dcd7d6a53f68e5b92ee0366750d75
Zac Yauney <zac.yauney@gmail.com>
2016-05-18 16:01:18 -0700

    Add django to requirements.txt, revert wsgi to original.  Should fix the problem

--- emerald/wsgi.py
+++ emerald/wsgi.py
@@ -8,16 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-# import sys
-
-# Add to python path
-# sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
-
-
-
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emerald.settings")
-import django.core.handlers.wsgi
-# application = get_wsgi_application()
-application = django.core.handlers.wsgi.WSGIHandler()
+
+application = get_wsgi_application()
+
