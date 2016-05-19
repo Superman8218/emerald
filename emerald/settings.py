12c3de0d45104f10263571eff87b0d17ec822d2f
Zac Yauney <zac.yauney@gmail.com>
2016-05-19 11:31:15 -0700

    Static fix

--- emerald/settings.py
+++ emerald/settings.py
@@ -117,10 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 )
