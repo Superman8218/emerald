30480640f453a5eaa3ac96e21b1a0f91d3212d1d
Zac Yauney <zac.yauney@gmail.com>
2016-05-18 17:30:13 -0700

    Change static settings

--- emerald/settings.py
+++ emerald/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
