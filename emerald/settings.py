23a34c3685b8323e5106881d2409d72653f567c2
Zac Yauney <zac.yauney@gmail.com>
2016-05-19 11:26:01 -0700

    Work on styles, rearranging static files

--- emerald/settings.py
+++ emerald/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'emerald/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
--- emerald/views.py
+++ emerald/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('base-page.html')
+    return render_to_response('emerald/base.html')
