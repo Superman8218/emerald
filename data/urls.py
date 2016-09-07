8d7f31bd7e145a5ff682d4e31a556c597e6ad235
Zac Yauney <zac.yauney@gmail.com>
2016-09-06 19:40:00 -0700

    Fbo detail and list improvements

--- data/urls.py
+++ data/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from views import detail
+from views import FboDetailView, FboImportView, FboListView
 
 urlpatterns = [
-        url(r'^detail', detail)
+        url(r'^fbo/(?P<pk>[0-9]*)/$', FboDetailView.as_view()),
+        url(r'^import/$', FboImportView),
+        url(r'^fbo/list/$', FboListView.as_view())
 ]
--- data/views.py
+++ data/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
+from django.views.generic import DetailView, ListView
 
 from models import FboMaster
 
+import FboImport
+
 # Create your views here.
 
-def detail(request):
-    return render_to_response(
-            'data/detail.html',
-            {
-                'data': FboMaster.objects.all()[:10],
-                'count':FboMaster.objects.count()
-            },
-            context_instance=RequestContext(request)
-    )
+def FboImportView(request):
+    FboImport.main()
+    return HttpResponse("Import was successful")
+
+class FboDetailView(DetailView):
+
+    model = FboMaster
+    template_name = 'data/fbo-detail.html'
+
+    # def get(self, request, fbo_master_id):
+        # fbo_master = FboMaster.objects.get(pk=fbo_master_id)
+        # return render(request, 'data/fbo-detail.html',
+                # {
+                    # 'fbo_master': fbo_master
+                # }
+            # )
+
+class FboListView(ListView):
+
+    model = FboMaster
+    template_name = 'data/fbo-list.html'
