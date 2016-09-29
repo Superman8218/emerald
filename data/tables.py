c09fe485584cc77b712100cce4cd25ef316a24f2
Zac Yauney <zac.yauney@gmail.com>
2016-09-28 17:09:59 -0700

    Use bootstrap table class for tables

--- data/tables.py
+++ data/tables.py
@@ -5,7 +5,9 @@ from models import FboMaster, Opportunity
 class FboMasterTable(tables.Table):
     class Meta:
         model = FboMaster
+        attrs = {'class':'table'}
 
 class OpportunityTable(tables.Table):
     class Meta:
         model = Opportunity
+        attrs = {'class':'table'}
