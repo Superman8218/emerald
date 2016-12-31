6dab92ab01e0a8e8ac0da1873ad9ccf15efa9236
Zac Yauney <zac.yauney@gmail.com>
2016-12-30 17:24:15 -0800

    Added url namespacing and magic search button, but right now the search button just goes to the list page.  Also got rid of the link to the opportunity page because that needs to be broken out into its own app

--- fbo/urls.py
+++ fbo/urls.py
@@ -5,10 +5,11 @@ from models import FboMaster
 from views import FboDetailView, FboImportView, FboListView, FboAddView, OpportunityDetailView, OpportunityListView
 
 urlpatterns = [
-        url(r'^fbo/(?P<pk>[0-9]*)/$', FboDetailView.as_view(), name='fbo-detail'),
-        url(r'^fbo/list/$', FboListView.as_view(), name='fbo-list'),
-        url(r'^import/$', FboImportView, name='fbo-import'),
-        url(r'^fbo/(?P<pk>[0-9]*)/add/$', FboAddView, name='fbo-add'),
-        url(r'^opportunity/detail/(?P<pk>[0-9]*)/$', OpportunityDetailView.as_view(), name='opportunity-detail'),
-        url(r'^opportunity/list', OpportunityListView.as_view(), name='opportunity-list')
+        url(r'^(?P<pk>[0-9]*)/$', FboDetailView.as_view(), name='detail'),
+        url(r'^list/$', FboListView.as_view(), name='list'),
+        url(r'^magic/$', FboListView.as_view(), name='magic'),
+        url(r'^import/$', FboImportView, name='import'),
+        url(r'^(?P<pk>[0-9]*)/add/$', FboAddView, name='add'),
+ #        url(r'^opportunity/detail/(?P<pk>[0-9]*)/$', OpportunityDetailView.as_view(), name='opportunity-detail'),
+ #        url(r'^opportunity/list', OpportunityListView.as_view(), name='opportunity-list')
 ]
