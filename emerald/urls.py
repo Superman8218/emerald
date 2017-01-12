c518456199bd1e85aa710eccb24a37136f94f922
Zac Yauney <zac.yauney@gmail.com>
2017-01-12 10:08:28 -0800

    Fix up some templates issues

--- emerald/urls.py
+++ emerald/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 
 from emerald.views import index
 from emerald.views import landing
-from emerald.views import test
 
 urlpatterns = [
         url(r'^admin/', admin.site.urls),
--- emerald/views.py
+++ emerald/views.py
@@ -1,21 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import RequestContext
-from fbo.models import FboMaster
 
 import pdb
 
 def index(request):
-    account_id = 0
-    if request.user.is_authenticated():
-        account_id = request.user.userprofile.account.id
-    return render(request, 'emerald/index.html', {
-        'account_id': account_id, 
-    })
-
-def test(request):
-    imp.main();
-    return HttpResponse("Test successful")
+    return render(request, 'emerald/index.html')
 
 def landing(request):
     return render(request, 'emerald/landing-page-email-list.html')
