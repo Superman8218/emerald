eb3968d123e5419c948f761a5b34cac9b44cc8ef
Zac Yauney <zac.yauney@gmail.com>
2017-05-02 15:00:45 -0700

    Added react to node

--- fbo/jirachi.py
+++ fbo/jirachi.py
@@ -1,2 +1,43 @@
-def parse_file(file_path):
+import re
 
+tag_regex = re.compile(r'<[A-Z]+>')
+
+field_aliases = {
+    'date': handle_date,
+    'year': handle_year,
+    'zip' : 'zip_code',
+    'classcod' : 'class_code',
+    'offadd' : 'office_address',
+    'respdate' : handle_respdate,
+    'archdate' : 'archive_date',
+    'contact' : handle_contact,
+    'desc' : 'description',
+}
+
+def extract_tag(line):
+    tag_end_index = line.find('>')
+    if tag_end_index != -1:
+        return line[1:tag_end_index]
+
+def get_tag_complement(tag):
+    return '</{0}>'.format(tag)
+
+def parse_file(file_path):
+    current_tag = ''
+    complement = ''
+    previous_line = ''
+    master = None
+    with open(file_path, 'r') as f:
+        lines = [line.strip('\n') for line in list(f)]
+        for index in xrange(0, len(lines)):
+            line = lines[index]
+            if not line:
+                continue
+            if not current_tag:
+                current_tag = extract_tag(line)
+                complement = get_tag_complement(current_tag)
+                master = Master()
+            elif line == complement:
+                current_tag = ''
+            elif tag_regex.match(line):
+                pass
--- fbo/models.py
+++ fbo/models.py
@@ -11,23 +11,34 @@ class FboMaster(models.Model):
 
     solicitation_type = models.CharField(max_length=20, choices=choices.SOLICITATION_TYPES)
     date = models.DateField(null=True)
+    zip_code = models.CharField(max_length=30) #zip
+    class_code = models.CharField(max_length=10) #classcod
+    naics = models.CharField(max_length=10)
+    office_address = models.CharField(max_length=400) #offadd
+    subject = models.CharField(max_length=400)
+    solnbr = models.CharField(max_length=100)
+    response_date = models.DateField(null=True) #respdate
+    archive_date = models.DateField(null=True) #archdate
+    contacts = models.ManyToManyField(Contact) #contact
+    description = models.TextField() #desc
+    link = models.URLField(null=True)
+    email = models.EmailField(null=True)
+    # links - array of links to documents
+    # files - array of files
     agency = models.CharField(max_length=200)
     office = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
-    zip_code = models.CharField(max_length=30)
-    class_code = models.CharField(max_length=10)
-    naics = models.CharField(max_length=10)
-    office_address = models.CharField(max_length=400)
-    subject = models.CharField(max_length=400)
-    solnbr = models.CharField(max_length=100)
-    response_date = models.DateField(null=True)
-    contacts = models.ManyToManyField(Contact)
-    description = models.TextField()
     url = models.URLField()
     setaside = models.CharField(max_length=100)
     pop_country = models.CharField(max_length=50)
     pop_address = models.CharField(max_length=100)
     pop_zip = models.CharField(max_length=30)
+    award_number = models.CharField(max_length=30)
+    award_amount = models.IntegerField(null=True)
+    award_date = models.DateField(null=True)
+    line_number = models.CharField(max_length=30)
+    ntype = models.CharField(max_length=30)
+
 
     objects = FboMasterManager()
 
