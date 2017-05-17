532c2cefac292df3fb074937756eef9bc6adb079
Zac Yauney <zac.yauney@gmail.com>
2017-05-16 17:33:28 -0700

    Work on contacts parsing

--- fbo/parse_helpers.py
+++ fbo/parse_helpers.py
@@ -2,6 +2,8 @@ import datetime
 # from models import FboMaster
 from contact.models import Contact
 
+import re
+
 import pdb
 
 def date(master, line):
@@ -93,31 +95,135 @@ class Tokenizer():
 
         return tokens
 
-# Accepts a Tokenizer object
-def extract_contacts(tokenizer):
-    contacts = []
+# # Accepts a Tokenizer object
+# def extract_contacts(tokenizer):
+    # contacts = []
 
-    contact = Contact()
-    current_token = tokenizer.next()
+    # contact = Contact()
+    # current_token = tokenizer.next()
 
-    # Discard the first one if it is 'Name', because we are assuming the first is a name
-    if (current_token == 'Name'):
-        current_token = tokens.next()
+    # # Discard the first one if it is 'Name', because we are assuming the first is a name
+    # if (current_token == 'Name'):
+        # current_token = tokens.next()
 
-    name = []
-    while (tokenizer.peek() != ','):
-        name.append(tokenizer.next())
+    # name = []
+    # while (tokenizer.peek() != ','):
+        # name.append(tokenizer.next())
 
-    if len(name) == 1:
-        #Special Case
-        pass
+    # if len(name) == 1:
+        # #Special Case
+        # pass
 
-    else:
-        while (tokenizer.peek() != ','):
-            name.append(tokenizer.next())
+    # else:
+        # while (tokenizer.peek() != ','):
+            # name.append(tokenizer.next())
 
 
-    return contacts
+    # return contacts
+
+# Utilities
+
+def contains_alpha_and_num(s):
+    alpha = False
+    num = False
+    for char in s:
+        if isalpha(char):
+            alpha = True
+        elif isdigit(char):
+            num = True
+    return alpha and num
+
+
+phone_regex = re.compile(r'(?:\+([0-9]+)[ ,.-])?\(?([0-9]{3})\)?[ ,.-]?([0-9]{3})[ .,-]?([0-9]{4})')
+
+# Takes a regex match for a phone number and
+
+def clean_single_phone_number(phone_match):
+    return ''.join([element for element in phone_match.groups() if element is not None])
+
+def clean_phone_numbers(line):
+    return re.sub(phone_regex, clean_single_phone_number, line)
+
+# Takes a single person and formats the names correctly
+def clean_names(person):
+    tokens = ','.split(person)[0].st
+    name_token = tokens[0]
+    if '@' in name_token: # This is just an email
+        return person
+    elif contains_alpha_and_num(name_token):
+        break_index = 0
+        for x in xrange(0, len(name_token)-1):
+            if isalpha(name_token[x]) and isdigit(name_token[x+1]) or isdigit(name_token[x]) and isalpha(name_token[x+1]):
+                break_index = x+1
+        if break_index == 0:
+            raise ValueError('Break index is 0')
+        fixed_name_token = name_token[:break_index] + ',' + name_token[break_index:]
+        person = person.replace(name_token, fixed_name_token)
+
+    # No need to go further if only one token
+
+    if len(tokens) == 1:
+        return person
+
+    # Last, First M.
+    if ' ' not in name_token:
+        current_name = tokens[0] + ', ' + tokens[1]
+        fixed_name = tokens[1] + ' ' + tokens[0]
+        person = person.replace(current_name, fixed_name)
+
+    # Add the 'Name: ' tag
+    raise Exception()
+    return person
+
+# Takes a single person and puts commas in all the right places
+def add_commas(person):
+    pass
+
+
+# Takes a cleaned line and returns a list of all the people in it
+def get_people(line):
+    return '-'.split(line)
+
+# Takes a single person and returns a list of all the component fields
+def get_fields(person):
+    # Conditions where we want to return empty, we don't want this contact
+
+    return_empty = False
+    if 'FEDBID' in person.upper():
+        return_empty = True
+
+    if return_empty:
+        return ''
+
+    # Clean the names on the person
+    person = clean_names(person)
+
+    # Insert commas at the right places
+    person = add_commas(person)
+    return ','.split(person)
+
+# Takes a list of component fields and turns them into a contact
+def process_fields(fields):
+    # Give the names the correct title case
+    return ''
+
+def extract_contacts(line):
+    # Clean the line to get it looking the way that we want
+
+    # Replace all phone numbers or fax numbers with unformatted digits
+    line = clean_phone_numbers(line)
+
+    # Now we process the cleaned line
+
+    # Split the line into a list of different people, splitting at the '-' character
+    people = get_people(line)
+
+    # Split the people into a list of different fields, based on the commas
+    field_collections = [get_fields(person) for person in people if person]
+
+    # Process each collection of fields into a contact
+    for field_collection in field_collections:
+        process_fields(field_collection)
 
 def contact(master, line):
     tokenizer = Tokenizer(line)
--- fbo/test_contacts.py
+++ fbo/test_contacts.py
@@ -113,37 +113,14 @@ class TestExtractContacts(TestCase):
 
     def test_normal(self):
 
-        line = 'Tina L. Watsontarver, Contract Specialist, Email tina.watsontarver.1@us.af.mil - Jaclyn G. Rodriguez, Contracting Officer, Email jaclyn.rodriguez@us.af.mil'
-        tokens = [
-            'Tina',
-            'L.',
-            'Watsontarver',
-            ',',
-            'Contract',
-            'Specialist',
-            ',',
-            'Email',
-            'tina.watsontarver.1@us.af.mil',
-            '-',
-            'Jaclyn',
-            'G.',
-            'Rodriguez',
-            ',',
-            'Contracting',
-            'Officer',
-            ',',
-            'Email',
-            'jaclyn.rodriguez@us.af.mil'
-        ]
-
-        tokenizer = Tokenizer(tokens)
-
-        contacts = extract_contacts(tokenizer)
+        line = 'Tina L. Watsontarver, Contract Specialist, Email tina.watsontarver.1@us.af.mil, Phone (123) 456-7890 - Jaclyn G. Rodriguez, Contracting Officer, Email jaclyn.rodriguez@us.af.mil'
+        contacts = extract_contacts(line)
 
         tina = Contact(
             name='Tina L. Watsontarver',
             title='Contract Specialist',
-            email='tima.watsontarver.1@us.af.mil'
+            email='tima.watsontarver.1@us.af.mil',
+            phone='1234567890',
         )
 
         jaclyn = Contact(
@@ -156,3 +133,102 @@ class TestExtractContacts(TestCase):
 
         self.assertEqual(answer, contacts)
 
+    # def test_normal(self):
+
+        # line = 'Tina L. Watsontarver, Contract Specialist, Email tina.watsontarver.1@us.af.mil - Jaclyn G. Rodriguez, Contracting Officer, Email jaclyn.rodriguez@us.af.mil'
+        # tokens = [
+            # 'Tina',
+            # 'L.',
+            # 'Watsontarver',
+            # ',',
+            # 'Contract',
+            # 'Specialist',
+            # ',',
+            # 'Email',
+            # 'tina.watsontarver.1@us.af.mil',
+            # '-',
+            # 'Jaclyn',
+            # 'G.',
+            # 'Rodriguez',
+            # ',',
+            # 'Contracting',
+            # 'Officer',
+            # ',',
+            # 'Email',
+            # 'jaclyn.rodriguez@us.af.mil'
+        # ]
+
+        # tokenizer = Tokenizer(tokens)
+
+        # contacts = extract_contacts(tokenizer)
+
+        # tina = Contact(
+            # name='Tina L. Watsontarver',
+            # title='Contract Specialist',
+            # email='tima.watsontarver.1@us.af.mil'
+        # )
+
+        # jaclyn = Contact(
+            # name='Jaclyn G. Rodriguez',
+            # title='Contracting Officer',
+            # email='jaclyn.rodriguez@us.af.mil'
+        # )
+
+        # answer = [tina, jaclyn]
+
+        # self.assertEqual(answer, contacts)
+
+
+    # def test_name_phone(self):
+        # line = 'Test Person123-456-7890'
+        # tokens = [
+            # 'Test',
+            # 'Person',
+            # '123',
+            # '-',
+            # '456',
+            # '-',
+            # '7890',
+        # ]
+        # tokenizer = Tokenizer(tokens)
+        # contacts = extract_contacts(tokenizer)
+        # person = Contact(
+            # name='Test Person',
+            # phone='1234567890',
+        # )
+        # answer = [person]
+        # self.assertEqual(answer, contacts)
+
+    # def test_semicolon_phone(self):
+        # line = 'Carolyn M. Blake 817-978-4661  ; Kelly M Pippin 817-850-5574'
+        # tokens = [
+            # 'Carolyn'
+            # 'M.'
+            # 'Blake'
+            # '817'
+            # '-'
+            # '978'
+            # '-'
+            # '4661'
+            # ';'
+            # 'Kelly'
+            # 'M'
+            # 'Pippin'
+            # '817'
+            # '-'
+            # '850'
+            # '-'
+            # '5574'
+        # ]
+        # tokenizer = Tokenizer(tokens)
+        # contacts = extract_contacts(tokenizer)
+        # carolyn = Contact(
+            # name='Carolyn M. Blake',
+            # phone='8179784661',
+        # )
+        # kelly = Contact(
+            # name='Kelly M Pippin',
+            # phone='8178505774',
+        # )
+        # answer = [carolyn, kelly]
+        # self.assertEqual(answer, contacts)
